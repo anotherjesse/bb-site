fac25b27f51d081868872d4a52a2ac94c7338a64
Jesse Andrews <anotherjesse@gmail.com>
2009-10-04 14:54:53 -0700

    slight tweaks to app

--- bookburro.rb
+++ bookburro.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'sinatra'
+require 'restclient'
+require 'json'
+
+DB = 'http://127.0.0.1:5984/libraries'
 
 module BookBurro
   class App < Sinatra::Default
@@ -24,6 +28,16 @@ module BookBurro
 
     get '/libraries' do
       erb :libraries
+    end
+
+    get '/libraries/all' do
+      @data = JSON.parse(RestClient.get("#{DB}/_design/bookburro/_view/flat"))
+      erb :list
+    end
+
+    get '/libraries/:id' do
+      @data = JSON.parse(RestClient.get("#{DB}/#{params[:id]}"))
+      erb :show
     end
 
     get '/getting_started' do
